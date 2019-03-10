953e1f35aeda3504b1608cccaf3e43e1a000daa3
Anton2295 <MY_NAME@example.com>
2019-03-10 21:49:36 +0300

    feat:add function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    let result = new Object();
+
+
+    if (currency > 10000) {
+        result["error"] = "You are rich, my friend! We don't have so much coins for exchange";
+
+        return result;
+    }
+
+    const config = [["H", 50], ["Q", 25], ["D", 10], ["N", 5], ["P", 1]];
+
+    const NAME = 0;
+    const VALUE = 1;
+
+
+    for (let i = 0; i < config.length; i++) {
+        if (config[i][VALUE] <= currency) {
+            let result_ = parseInt(currency / config[i][VALUE]);
+
+            result[config[i][NAME]] = result_;
+
+            currency = currency % config[i][VALUE];
+
+           // console.log(config[i][NAME] + " " + config[i][VALUE] + " " + result_);
+
+        }
+
+    }
+
+    return result;
 }
+
+
+
+
+
